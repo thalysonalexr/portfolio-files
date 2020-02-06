881fc9b768fcdb0b15d11b6b238655fbf75754ad
Thalyson Rodrigues <thalysonrodrigues.dev@gmail.com>
2020-02-06 18:39:27 -0400

    update functions

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,14 @@
 const gulp = require('gulp');
 const image = require('gulp-image');
 const jsonminify = require('gulp-jsonminify');
- 
-gulp.task('image', function () {
-  gulp.src('./images/**')
+
+gulp.task('image', () => {
+  return gulp.src('./images/**')
     .pipe(image())
     .pipe(gulp.dest('./dist/images'));
 });
 
-gulp.task('jsonminify', function () {
+gulp.task('jsonminify', () => {
   return gulp.src('./data/**')
     .pipe(jsonminify())
     .pipe(gulp.dest('./data/compress'));
